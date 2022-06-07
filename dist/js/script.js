228f15bcd7a91d3e5b24460c295b6db0e4542313
Syahroni Rizki <63556406+syahronirizki@users.noreply.github.com>
2022-06-07 08:44:10 +0700

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,19 @@ window.onscroll = function () {
     header.classList.remove("navbar-fixed");
   }
 };
+
+// Copy Email
+function copyFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("emailku");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
